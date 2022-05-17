afde9b195874b3e4ab638d203aa498c11d41745c
Arthur Shaw <47256431+arthur-shaw@users.noreply.github.com>
2022-05-17 16:02:09 -0400

    Elargir les paramètres de projet afin d'élargir la fonctionnalité
    Besoin d'infos sur:
    
    - Entretiens à inclure
    - Plan d'échantillonnage pour suivre le progrès au niveau global et au niveau DR
    - Accès au serveur. Ne plus faire l'hypothèse que ces infos sont sur la machine et correctes.
    - Applications de collecte. Savoir quelles bases télécharger.
    - Fichiers principaux. Avoir les noms et, pour l'enquête communautaire, le nom de colonnes clé
    - Fichiers de référence pour le calcul de calories

--- creer_rapport_suivi.R
+++ creer_rapport_suivi.R
@@ -5,19 +5,73 @@
 rapport_dir <- ""
 
 rapport_params <- list(
+
+    # RÉPERTOIRE DU PROJET
+    proj_dir = rapport_dir,
+
     # PÉRIODE DU RAPPORT: DÉBUT ET FIN
     # pour les dates,  mettre dans le format ISO 8601: AAAA-MM-JJ
-    # par exemple, "2021-11-25" pour le 25 novembre, 2021
+    # par exemple "2021-11-25" pour le 25 novembre 2021
     rapport_debut   = "",
     rapport_fin     = "",
-    # RÉPERTOIRE DU PROGRAMME DE REJET
-    # le rapport puise les données du programme de rejet
-    rejet_proj_dir  = "",
-    # NOM DU FICHIER PRINCIPAL
+
+    # ENTRETIENS À INCLURE SELON LE STATUT
+    # Notez ce à quoi correspond chaque valeur de statut
+    # 65 RejectedBySupervisor
+    # 100 Completed
+    # 120 ApprovedBySupervisor
+    # 125 RejectedByHeadquarters
+    # 130 ApprovedByHeadquarters
+    statuses_to_review = c(100, 120, 130),
+
+    # ECHANTILLONAGE
+    # Veuillez fournir quelques informations qui permettront d'informer
+    # le progrès global de l'enquête et le progrès au niveau de chaque DR
+    # nombre de DR attendu
+    n_dr            = ,
+    # nombre de ménages attendu par DR selon le plan d'échantillonnage
+    n_menages_dr    = ,
+    # nombre de ménages de remplacement (plafond)
+    n_menages_remplacement = ,
+
+    # ACCES AU SERVEUR
+    serveur         = "",
+    utilisateur_api = "",
+    passe_api       = "",
+    workspace       = "",
+
+    # APPLICATIONS DE COLLECTE
+    # Nom des masques
+    masque_menage   = "",
+    masque_comm     = "",
+
+    # FICHIERS PRINCIPAUX PAR APPLI: NOM ET VARIABLES
+    # tâcher d'inclure l'exentension .dta.
+    # MENAGE
     # normalement, ça doit être "menage.dta", comme la valeur de défaut ici-bas
-    # tâcher d'inclure l'exentension  .dta.
-    main_file_dta   = "menage.dta",
-    workspace       = "primary",
+    menage_fichier  = "",
+    # COMMUNAUTAIRE
+    comm_fichier    = "",
+    grappe_var      = "grappe",
+    region_var      = "s00q01", # s00q01
+    departement_var = "s00q02", # s00q02
+    commune_var     = "s00q03", # s00q03
+    milieu_var      = "s00q04", # s00q04
+    dr_var          = "s00q06", # s00q06
+
+    # CALCUL DES CALORIES
+    # fichier calories
+    nom_fichier_calories = "",
+    # fichier facteurs
+    nom_fichier_facteurs = "",
+    facteurs_niv        = "", # valeurs: "national", "strate"
+    facteurs_prod_id    = "",
+    facteurs_region     = "", # si niveau national, mettre NULL (sans guillemets)
+    facteurs_milieu     = "", # si niveau national, mettre NULL (sans guillemets)
+    facteurs_unite      = "",
+    facteurs_taille     = "",
+    facteurs_poids      = "",
+
     # CHEFS D'ÉQUIPE À EXCLURE DU RAPPORT
     # parfois, il existe des chefs fictifs ou des rélicats de la formation
     # si tel et le cas, fournir une liste délimitée par virgule des noms (e.g., "Chef1, Chef2")
